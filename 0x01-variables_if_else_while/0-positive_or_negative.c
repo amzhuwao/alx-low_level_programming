3b0d80cf901cf03a5df36faa82a0663bae99988d
Aubrey Zhuwao <amzhuwao@gmail.com>
2023-08-11 06:30:55 -0700

    task 0 after betty :(

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This function prints "Hello, world!" to the console.
+ *
+ * Return: Always 0 (Success).
+ */
 int main(void)
 {
 	int n;
@@ -10,13 +17,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if(n > 0){
+	if (n > 0)
+	{
 		printf("%d is a positive number\n", n);
 	}
-	else if(n==0){
+	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
 	}
-	else{
+	else
+	{
 		printf("%d is negative\n", n);
 	}
 	return (0);
