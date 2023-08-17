407e6d250d7f7875cb95a023af3138b9810e4745
Aubrey Zhuwao <amzhuwao@gmail.com>
2023-08-17 23:49:39 +0200

    Done taska 1 - 10

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks if a chatactor is between 0 & 9
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - gives the multiple of variable a & b
+ * @a: local variable
+ * @b: local variable
+ * Return: a times b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
