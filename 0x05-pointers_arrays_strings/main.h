cbfe9212f3ef9d479dd9c483b6086152c4ce8738
Aubrey Zhuwao <amzhuwao@gmail.com>
2023-08-23 04:18:58 +0200

    added prototypes to main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,17 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to be written
- *
- * Return: On success, 1. On error, -1 is returned,
- * and errno is set appropriately.
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+int _strlen(char *s);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
+#endif /* MAIN_H */
