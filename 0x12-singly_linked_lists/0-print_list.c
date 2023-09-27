5e9678a26efe9c0a251d0139d2a50e706611eb93
=aubrey zhuwao <amzhuwao@gmail.com>
2023-09-27 12:35:20 +0200

    done print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,18 +6,19 @@
  * Return: number of elements in the list.
  */
 
-size_t print_list(const list_t *h){
-	size_t nelem;
-	nelem = 0;
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
 
-	while (h != NULL){
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+	while (h)
+	{
+		if (h->str)
+			printf("%s\n", h->str);
 		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[0] (nil)\n");
 
-	h = h->next;
-	nelem++;
+		count++;
+		h = h->next;
 	}
-	return (nelem);
+	return (count);
 }
